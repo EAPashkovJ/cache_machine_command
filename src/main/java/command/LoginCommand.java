a8f41f433d092ffb3f537f9a2f836434c68434f3
Evgenii <eapashkov@gmail.com>
2022-11-03 16:28:20 +0300

    Вход для работы с банкоматом

--- src/main/java/command/LoginCommand.java
+++ src/main/java/command/LoginCommand.java
@@ -1,11 +1,30 @@
 package command;
 
+import maitain.ConsoleOutput;
+import resources.AllowedCard;
+
+
 public class LoginCommand implements Command {
+    AllowedCard allowedCard = new AllowedCard();
 
-    // верификация с карты и пин код
-
+    /**
+     * Login class check allowed cardholders of bank
+     */
     @Override
     public void execute() {
+        while (true) {
+            ConsoleOutput.writeString("Введите номер карты:");
 
+            String cardNumber = ConsoleOutput.readString();
+            if (allowedCard.BaseOfCards().containsKey(cardNumber)) {
+                ConsoleOutput.writeString("Введите pin-code");
+                String pin = ConsoleOutput.readString();
+//                if (allowedCard.BaseOfCards().containsValue(pin)) {
+                if (allowedCard.BaseOfCards().get(cardNumber).equals(pin)) {
+                    ConsoleOutput.writeString("Добро пожаловать!");
+                    break;
+                } else ConsoleOutput.writeString("Неверный pin-code");
+            } else ConsoleOutput.writeString("Неверный номер карты");
+        }
+        }
     }
-}
