36e8070a173ab7d0841fdeac8a6b7f139a447a83
Evgenii <eapashkov@gmail.com>
2022-11-03 16:28:01 +0300

    Завершение работы с банком

--- src/main/java/command/ExitCommand.java
+++ src/main/java/command/ExitCommand.java
@@ -1,10 +1,16 @@
 package command;
 
+import maitain.ConsoleOutput;
+
 public class ExitCommand implements Command {
 
 
     @Override
     public void execute() {
+        ConsoleOutput.writeString("Для выхода из банка введите \" 1 \"  или \" 2\" для отмены действия");
 
+        String answer = ConsoleOutput.readString();
+        if(answer.equals("1"))
+            ConsoleOutput.writeString("До свидания!");
     }
 }
